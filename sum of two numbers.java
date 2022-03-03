d1ea89f57f4cb94b2d4a9f677acf7893c582c4a4
RaniaMesak <97768669+RaniaMesak@users.noreply.github.com>
2022-03-03 16:03:41 -0600

    Update and rename sum of two numbers to sum of two numbers.java

--- sum of two numbers.java
+++ sum of two numbers.java
@@ -5,10 +5,10 @@ public class Main {
   {
     Scanner input = new Scanner (System.in);
     System.out.print("Input the first number: ");
-    int num1 = input.nextInt();
+    int firstNumber = input.nextInt();
     System.out.print("Input the second number: ");
-    int num2 = input.nextInt();
-    int sum = num1 + num2;
+    int secondNumber = input.nextInt();
+    int sum = firstNumber + secondNumber;
     System.out.println();
     System.out.println("Sum: "+sum);
   }
